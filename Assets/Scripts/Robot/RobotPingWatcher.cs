431fe9af03ee4e973a64efb40f2e11c691d061e2
Fabien Danieau <fabien.danieau@pollen-robotics.com>
2023-05-10 16:10:40 +0200

    bug #02: deactivate ping for Android

--- Assets/Scripts/Robot/RobotPingWatcher.cs
+++ Assets/Scripts/Robot/RobotPingWatcher.cs
@@ -7,11 +7,11 @@ namespace TeleopReachy
 {
     public class RobotPingWatcher : MonoBehaviour
     {
-        private const int QUEUE_SIZE = 5;
+        private const int QUEUE_SIZE = 10;
 
         private const float REFRESH_REQ_SEC = 1;
 
-        public const int THRESHOLD_LOW_QUALITY_PING = 45;
+        public const int THRESHOLD_LOW_QUALITY_PING = 70;
 
         private float mean_ping = 0;
 
@@ -22,8 +22,11 @@ namespace TeleopReachy
         // Start is called before the first frame update
         void Start()
         {
+            //UnityEngine.Ping always return -1 on android
+#if !UNITY_ANDROID
             string robot_ip = PlayerPrefs.GetString("robot_ip");
-            if(robot_ip != "localhost" && robot_ip != Robot.VIRTUAL_ROBOT_IP) pingCheck = StartCoroutine(MeanPing(robot_ip));
+            if (robot_ip != "localhost" && robot_ip != Robot.VIRTUAL_ROBOT_IP) pingCheck = StartCoroutine(MeanPing(robot_ip));
+#endif
         }
 
         void OnDestroy()
@@ -38,20 +41,26 @@ namespace TeleopReachy
             while (true)
             {
                 Ping p = new Ping(ip);
-
+                isUnstable = false;
                 yield return new WaitForSeconds(REFRESH_REQ_SEC);
 
-                if(p.isDone) lastPingTimes.Enqueue(p.time);
-                else lastPingTimes.Enqueue((int)REFRESH_REQ_SEC * 1000);
+                yield return new WaitUntil(() => p.isDone);
+
+                if (p.time > -1)
+                {
+                    lastPingTimes.Enqueue(p.time);
+                }
+                else
+                {
+                    lastPingTimes.Enqueue((int)REFRESH_REQ_SEC * 1000);
+                    isUnstable = true;
+                }
 
                 if (lastPingTimes.Count > QUEUE_SIZE) lastPingTimes.Dequeue();
 
                 float mean = 0;
-                isUnstable = false;
                 foreach (int obj in lastPingTimes)
                 {
-                    if (obj > THRESHOLD_LOW_QUALITY_PING)
-                        isUnstable = true;
                     mean += obj;
                 }
                 mean_ping = mean / lastPingTimes.Count;
