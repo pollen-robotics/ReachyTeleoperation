d384eb7964a9332f29f927f078aeb309409352e5
Fabien Danieau <fabien.danieau@pollen-robotics.com>
2023-07-11 16:41:31 +0200

    enhancement #11 : hide reachy and mirror when resuming message is displayed. changed the message to ready to resume teleoperation

--- Assets/Scripts/Manager/HeadsetRemovedInMirrorManager.cs
+++ Assets/Scripts/Manager/HeadsetRemovedInMirrorManager.cs
@@ -9,8 +9,9 @@ namespace TeleopReachy
     {
         [SerializeField]
         private Transform headsetRemovedMessage;
-
         private bool wasUserPresent;
+        public const string mirrorLayer = "Mirror";
+        public const string reachyLayer = "Reachy";
 
         // Start is called before the first frame update
         void Start()
@@ -22,11 +23,15 @@ namespace TeleopReachy
         void ShowResetPosition()
         {
             headsetRemovedMessage.gameObject.SetActive(true);
+            Camera.main.cullingMask &= ~(1 << LayerMask.NameToLayer(mirrorLayer));
+            Camera.main.cullingMask &= ~(1 << LayerMask.NameToLayer(reachyLayer));
         }
 
         public void HideResetPosition()
         {
             headsetRemovedMessage.gameObject.SetActive(false);
+            Camera.main.cullingMask |= 1 << LayerMask.NameToLayer(mirrorLayer);
+            Camera.main.cullingMask |= 1 << LayerMask.NameToLayer(reachyLayer);
         }
     }
 }
