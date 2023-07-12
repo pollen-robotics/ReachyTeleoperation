e8844129e7631b7075fb9c15314d277393f623b9
Fabien Danieau <fabien.danieau@pollen-robotics.com>
2023-07-12 10:42:34 +0200

    enhancement #19: text indicating a button is modified according to controller

---
+++ Assets/Scripts/UI/TextButtonControllerModifier.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TeleopReachy
+{
+    public class TextButtonControllerModifier : MonoBehaviour
+    {
+        private Text textToChange;
+        public ControllersManager controllers;
+        // Start is called before the first frame update
+        void Start()
+        {
+            textToChange = GetComponent<Text>();
+            controllers = ActiveControllerManager.Instance.ControllersManager;
+            textToChange.text = ChangeTextAccordingToController(textToChange.text);
+        }
+
+        public string ChangeTextAccordingToController(string stringToChange)
+        {
+            switch (controllers.controllerDeviceType)
+            {
+                case ControllersManager.SupportedDevices.Oculus:
+                    {
+                        stringToChange = stringToChange.Replace("<primRight>", "A");
+                        stringToChange = stringToChange.Replace("<primLeft>", "X");
+                        break;
+                    }
+                case ControllersManager.SupportedDevices.HTCVive:
+                    {
+                        stringToChange = stringToChange.Replace("<primRight>", "Right menu");
+                        stringToChange = stringToChange.Replace("<primLeft>", "Left menu");
+                        break;
+                    }
+                case ControllersManager.SupportedDevices.ValveIndex:
+                    {
+                        stringToChange = stringToChange.Replace("<primRight>", "Right A");
+                        stringToChange = stringToChange.Replace("<primLeft>", "Left A");
+                        break;
+                    }
+            }
+            return stringToChange;
+        }
+    }
+}
