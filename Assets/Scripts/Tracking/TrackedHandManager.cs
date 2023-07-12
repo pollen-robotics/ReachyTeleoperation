38b0d7a889adcede89759300f87db1fd026c93fa
glannuzel <gaelle.lannuzel@gmail.com>
2023-07-12 13:46:59 +0200

    Add TrackedHandManager on tracked objects.

---
+++ Assets/Scripts/Tracking/TrackedHandManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using System;
+using UnityEngine.XR.Interaction.Toolkit;
+using Reachy.Sdk.Kinematics;
+
+namespace TeleopReachy
+{
+    public class TrackedHandManager : MonoBehaviour
+    {
+        public ArmSide side_id;
+
+        void Start()
+        {
+            ControllersManager.Instance.event_OnDevicesUpdate.AddListener(DefineTrackedHandOrientation);
+        }
+
+        private void DefineTrackedHandOrientation()
+        {
+            if(ControllersManager.Instance.controllerDeviceType == ControllersManager.SupportedDevices.Oculus)
+            {
+                transform.localPosition = new Vector3(0, -0.05f, 0);
+                UnityEngine.Quaternion targetRotation = new UnityEngine.Quaternion();
+                if(side_id == ArmSide.Left) targetRotation.eulerAngles = new Vector3(-60, 30, 0);
+                else targetRotation.eulerAngles = new Vector3(-60, -30, 0);
+                transform.rotation = targetRotation;
+            }
+
+            if(ControllersManager.Instance.controllerDeviceType == ControllersManager.SupportedDevices.ValveIndex)
+            {
+                transform.localPosition = new Vector3(0, -0.05f, 0);
+                UnityEngine.Quaternion targetRotation = new UnityEngine.Quaternion();
+                if(side_id == ArmSide.Left) targetRotation.eulerAngles = new Vector3(-60, 30, 0);
+                else targetRotation.eulerAngles = new Vector3(-60, -30, 0);
+                transform.rotation = targetRotation;
+            }
+
+            if(ControllersManager.Instance.controllerDeviceType == ControllersManager.SupportedDevices.HTCVive)
+            {
+                transform.localPosition = new Vector3(0, -0.05f, 0);
+                UnityEngine.Quaternion targetRotation = new UnityEngine.Quaternion();
+                if(side_id == ArmSide.Left) targetRotation.eulerAngles = new Vector3(-60, 30, 0);
+                else targetRotation.eulerAngles = new Vector3(-60, -30, 0);
+                transform.rotation = targetRotation;
+            }
+        }
+    }
+}
