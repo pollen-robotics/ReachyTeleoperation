14fa2c9ee732a12528e3a162c3d3db2e3a70cc5a
Fabien Danieau <fabien.danieau@pollen-robotics.com>
2023-07-12 18:09:40 +0200

    enhancement #24: change multiple if to switch

--- Assets/Scripts/Tracking/TrackedHandManager.cs
+++ Assets/Scripts/Tracking/TrackedHandManager.cs
@@ -19,31 +19,35 @@ namespace TeleopReachy
 
         private void DefineTrackedHandOrientation()
         {
-            if(ControllersManager.Instance.controllerDeviceType == ControllersManager.SupportedDevices.Oculus)
+            switch (ControllersManager.Instance.controllerDeviceType)
             {
-                transform.localPosition = new Vector3(0, -0.05f, 0);
-                UnityEngine.Quaternion targetRotation = new UnityEngine.Quaternion();
-                if(side_id == ArmSide.Left) targetRotation.eulerAngles = new Vector3(-60, 30, 0);
-                else targetRotation.eulerAngles = new Vector3(-60, -30, 0);
-                transform.rotation = targetRotation;
-            }
-
-            if(ControllersManager.Instance.controllerDeviceType == ControllersManager.SupportedDevices.ValveIndex)
-            {
-                transform.localPosition = new Vector3(0, 0, 0);
-                UnityEngine.Quaternion targetRotation = new UnityEngine.Quaternion();
-                if(side_id == ArmSide.Left) targetRotation.eulerAngles = new Vector3(0, 0, 20);
-                else targetRotation.eulerAngles = new Vector3(0, 0, -20);
-                transform.rotation = targetRotation;
-            }
-
-            if(ControllersManager.Instance.controllerDeviceType == ControllersManager.SupportedDevices.HTCVive)
-            {
-                transform.localPosition = new Vector3(0, 0, 0);
-                UnityEngine.Quaternion targetRotation = new UnityEngine.Quaternion();
-                if(side_id == ArmSide.Left) targetRotation.eulerAngles = new Vector3(0, 0, 0);
-                else targetRotation.eulerAngles = new Vector3(0, 0, 0);
-                transform.rotation = targetRotation;
+                case ControllersManager.SupportedDevices.Oculus:
+                    {
+                        transform.localPosition = new Vector3(0, -0.05f, 0);
+                        UnityEngine.Quaternion targetRotation = new UnityEngine.Quaternion();
+                        if (side_id == ArmSide.Left) targetRotation.eulerAngles = new Vector3(-60, 30, 0);
+                        else targetRotation.eulerAngles = new Vector3(-60, -30, 0);
+                        transform.rotation = targetRotation;
+                        break;
+                    }
+                case ControllersManager.SupportedDevices.ValveIndex:
+                    {
+                        transform.localPosition = new Vector3(0, 0, 0);
+                        UnityEngine.Quaternion targetRotation = new UnityEngine.Quaternion();
+                        if (side_id == ArmSide.Left) targetRotation.eulerAngles = new Vector3(0, 0, 20);
+                        else targetRotation.eulerAngles = new Vector3(0, 0, -20);
+                        transform.rotation = targetRotation;
+                        break;
+                    }
+                case ControllersManager.SupportedDevices.HTCVive:
+                    {
+                        transform.localPosition = new Vector3(0, 0, 0);
+                        UnityEngine.Quaternion targetRotation = new UnityEngine.Quaternion();
+                        if (side_id == ArmSide.Left) targetRotation.eulerAngles = new Vector3(0, 0, 0);
+                        else targetRotation.eulerAngles = new Vector3(0, 0, 0);
+                        transform.rotation = targetRotation;
+                        break;
+                    }
             }
         }
     }
